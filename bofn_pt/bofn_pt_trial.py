78480b51e560d9ed2ec6e41863e7b60525082c71
pb482 <pb482@cornell.edu>
2022-07-08 18:21:46 -0400

    Updated files

--- bofn_pt/bofn_pt_trial.py
+++ bofn_pt/bofn_pt_trial.py
@@ -18,7 +18,7 @@ from gpytorch.mlls import ExactMarginalLogLikelihood
 from bofn_pt.models.gp_network_partial import GaussianProcessNetworkPartial
 from bofn_pt.utils.posterior_mean import PosteriorMean
 from bofn_pt.acquisition_function_optimization.optimize_acqf import optimize_acqf_and_get_suggested_point
-from bofn_pt.acquisition_function_optimization.thompson_acqf import ThompsonSampling
+from bofn_pt.acquisition_function_optimization.thompson_fn_acqf import ThompsonSamplingFN
 from botorch.models.transforms import Standardize
 from botorch.optim.initializers import initialize_q_batch_nonneg
 
@@ -36,6 +36,7 @@ dtype = torch.double
 def bofn_pt_trial(
         problem: str,
         first_batch_algo: str,
+        Constant_Liar: str,
         second_batch_algo: str,
         trial: int,
         n_init_evals: int,
@@ -50,9 +51,9 @@ def bofn_pt_trial(
     script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
 
     if first_batch_algo is not None:
-        results_folder = script_dir + "/results/" + problem + "/" + first_batch_algo + "_" + second_batch_algo +"/"
+            results_folder = script_dir + "/results/" + problem + "/" + first_batch_algo +"-"+ Constant_Liar + "_" + second_batch_algo + "/"
     else:
-        results_folder = script_dir + "/results/" + problem + "/" + second_batch_algo + "/"
+            results_folder = script_dir + "/results/" + problem + "/" + second_batch_algo + "/"
 
     # Initial evaluations
     X = generate_initial_design(
@@ -64,6 +65,7 @@ def bofn_pt_trial(
     first_layer_input = X
     first_layer_output = network_output_at_X[:,:-1]
     n_first_layer_nodes = first_layer_output.shape[1]
+
     # Current Best Objective value
     best_obs_val = objective_at_X.max().item()
 
@@ -75,6 +77,7 @@ def bofn_pt_trial(
     for iteration in range(init_batch_id, n_bo_iter+1):
         print("Experiment: " + problem)
         print("First Batch algo: " + first_batch_algo)
+        print("Constant Liar: " + str(Constant_Liar))
         print("Second Batch algo: "+ second_batch_algo)
         print("Trial: " + str(trial))
         print("Iteration: " + str(iteration))
@@ -86,13 +89,7 @@ def bofn_pt_trial(
             if second_batch_algo == "RAND":
                 second_batch = torch.rand([n_second_batch, input_dim])
             elif second_batch_algo == "TS-Whole":
-                second_batch = generate_batch_thompson(first_layer_input=first_layer_input,
-                first_layer_output=first_layer_output,second_layer_input=first_layer_output,
-                second_layer_output=objective_at_X,batch_size=n_second_batch,n_candidates=500,
-                network_to_objective_transform=network_to_objective_transform)
-                y_at_new_point = function_network(second_batch)[:, :-1].clone()
-                first_layer_input = torch.cat((first_layer_input,second_batch),0)
-                first_layer_output = torch.cat((first_layer_output,y_at_new_point),0)
+                print("To be filled")
             else:
                 print("Invalid algorithm")
                 break
@@ -130,18 +127,19 @@ def bofn_pt_trial(
                 else:
                     first_batch = torch.cat((first_batch,new_point),0)
                     temp_second_layer_input = torch.cat((temp_second_layer_input,y_at_new_point_norm),0)
-                if first_batch_algo in ["EIFN-CLMAX","TS-CLMAX"]:
-                    CL = objective_at_X.max()
-                    second_layer_input = torch.cat((second_layer_input,y_at_new_point),0)
-                    second_layer_output = torch.cat((second_layer_output,CL.reshape(1,1)),0)
-                elif first_batch_algo in ["EIFN-CLMIN","TS-CLMIN"]:
-                    CL = objective_at_X.min()
-                    second_layer_input = torch.cat((second_layer_input,y_at_new_point),0)
-                    second_layer_output = torch.cat((second_layer_output,CL.reshape(1,1)),0)
-                elif first_batch_algo in ["EIFN-CLMEAN","TS-CLMEAN"]:
-                    CL = model_second_layer.posterior(y_at_new_point_norm).mean.item()
-                    second_layer_input = torch.cat((second_layer_input,y_at_new_point),0)
-                    second_layer_output = torch.cat((second_layer_output,torch.tensor(CL).reshape(1,1)),0)
+                if Constant_Liar != "NoCL":
+                    if Constant_Liar == "CLMAX":
+                        CL = objective_at_X.max()
+                        second_layer_input = torch.cat((second_layer_input,y_at_new_point),0)
+                        second_layer_output = torch.cat((second_layer_output,CL.reshape(1,1)),0)
+                    elif Constant_Liar == "CLMIN":
+                        CL = objective_at_X.min()
+                        second_layer_input = torch.cat((second_layer_input,y_at_new_point),0)
+                        second_layer_output = torch.cat((second_layer_output,CL.reshape(1,1)),0)
+                    elif Constant_Liar == "CLMEAN":
+                        CL = model_second_layer.posterior(y_at_new_point_norm).mean.item()
+                        second_layer_input = torch.cat((second_layer_input,y_at_new_point),0)
+                        second_layer_output = torch.cat((second_layer_output,torch.tensor(CL).reshape(1,1)),0)
             second_batch = get_second_batch(first_batch = first_batch, temp_second_layer_input=temp_second_layer_input,
             model_second_layer=model_second_layer, best_obs_val = best_obs_val,
             n_second_batch = n_second_batch,second_batch_algo=second_batch_algo)
@@ -180,14 +178,12 @@ def get_first_batch(
         network_to_objective_transform: Callable
 )-> Tensor:
     input_dim = first_layer_input.shape[-1]
-
-    # Model
-    model = GaussianProcessNetworkPartial(train_X = first_layer_input,
-                                          train_Y_output_first_layer = first_layer_output,
-                                          train_Y_input_second_layer = second_layer_input,
-                                          train_Z = second_layer_output)
-    qmc_sample = SobolQMCNormalSampler(num_samples=128)
-    if first_batch_algo in ["EIFN-NoCL","EIFN-CLMAX","EIFN-CLMIN","EIFN-CLMEAN"]:
+    if first_batch_algo == "EIFN":
+        model = GaussianProcessNetworkPartial(train_X = first_layer_input,
+                                        train_Y_output_first_layer = first_layer_output,
+                                        train_Y_input_second_layer = second_layer_input,
+                                        train_Z = second_layer_output)
+        qmc_sample = SobolQMCNormalSampler(num_samples=128)
         acquisition_function = qExpectedImprovement(
             model = model,
             best_f = second_layer_output.max().item(),
@@ -204,43 +200,34 @@ def get_first_batch(
             bounds=torch.tensor([[0. for i in range(input_dim)],
                                  [1. for i in range(input_dim)]]),
             batch_size=1,
-            posterior_mean = posterior_mean_function
+            posterior_mean = posterior_mean_function,
         )
-    elif first_batch_algo in ["TS-NoCL", "TS-CLMAX", "TS-CLMIN", "TS-CLMEAN"]:
-        random_seeds_first_layer=torch.randint(low=1,high=10000,size=n_first_layer_nodes)
-        random_seed_second_layer=torch.randint(low=1,high=10000,size=1)
+    elif first_batch_algo == "TSFN":
         first_layer_GPs = [None for i in range(n_first_layer_nodes)]
         for i in range(n_first_layer_nodes):
-            first_layer_GPs[i] = SingleTaskGP(train_X=first_layer_input,train_Y=first_layer_output,outcome_transform=Standardize(m=1))
+            first_layer_GPs[i] = SingleTaskGP(train_X=first_layer_input,train_Y=first_layer_output[...,[i]],outcome_transform=Standardize(m=1))
         second_layer_input_norm = second_layer_input.clone()
         normal_lower = [None for i in range(n_first_layer_nodes)]
         normal_upper = [None for i in range(n_first_layer_nodes)]
         for i in range(n_first_layer_nodes):
-            normal_lower[i] = torch.min(network_output_at_X[..., i])
-            normal_upper[i] = torch.max(network_output_at_X[..., i])
+            normal_lower[i] = torch.min(second_layer_input[..., i])
+            normal_upper[i] = torch.max(second_layer_input[..., i])
             second_layer_input_norm[..., i] = (second_layer_input_norm[..., i] - normal_lower[i]) / (
                         normal_upper[i] - normal_lower[i])
         second_layer_GP  = SingleTaskGP(train_X=second_layer_input_norm, train_Y=second_layer_output,
                                           outcome_transform=Standardize(m=1))
-        acquisition_function = ThompsonSampling(first_layer_GPs=first_layer_GPs,
-                                                random_seeds_first_layer=random_seeds_first_layer,
+        acquisition_function = ThompsonSamplingFN(first_layer_GPs=first_layer_GPs,
                                                 second_layer_GP=second_layer_GP,
-                                                random_seed_second_layer=random_seed_second_layer,
-                                                n_first_layer_nodes=n_first_layer_nodes,normal_lower=normal_lower,
-                                                normal_upper=normal_upper)
-        def fitness_func(X,solution_idx):
-            fitness_val = acquisition_function.forward(X=X).item()
-            return fitness_val
-        gene_space = [{'low': 0, 'high': 1} for i in range(input_dim)]
-        ga_instance = pygad.GA(num_generations=50,
-                               sol_per_pop=10,
-                               num_genes=input_dim,
-                               num_parents_mating=2,
-                               fitness_func=fitness_func,
-                               gene_space=gene_space)
-        ga_instance.run()
-        new_first_batch = ga_instance.best_solution()[0]
-        new_first_batch = torch.from_numpy(new_first_batch).reshape(1,input_dim)
+                                                n_first_layer_nodes=n_first_layer_nodes,
+                                                normal_lower=normal_lower,normal_upper=normal_upper)
+        new_first_batch = optimize_acqf_and_get_suggested_point(
+            acq_func = acquisition_function,
+            bounds=torch.tensor([[0. for i in range(input_dim)],
+                                 [1. for i in range(input_dim)]]),
+            batch_size=1,
+            batch_limit=1,
+            init_batch_limit=1
+        )
     else:
         print("Invalid algorithm!")
     return new_first_batch
