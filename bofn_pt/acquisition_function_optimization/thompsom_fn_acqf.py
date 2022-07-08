1fb59f078aa2a0c768393dfbe37857be5f116fb5
pb482 <pb482@cornell.edu>
2022-07-08 14:36:31 -0400

    Updated files

---
+++ bofn_pt/acquisition_function_optimization/thompsom_fn_acqf.py
@@ -0,0 +1,46 @@
+import torch
+from torch import Tensor
+from botorch.fit import fit_gpytorch_model
+from botorch.models import SingleTaskGP
+from botorch.test_functions import Hartmann
+from gpytorch.mlls import ExactMarginalLogLikelihood
+from botorch.utils.gp_sampling import get_gp_samples
+class ThompsonSampling:
+    def __init__(
+        self,
+        first_layer_GPs,
+        random_seeds_first_layer,
+        second_layer_GP,
+        random_seed_second_layer,
+        n_first_layer_nodes,
+        normal_lower,
+        normal_upper
+    ) -> None:
+        r"""Thompson Sampling Acquisition Function.
+
+        Args:
+            first_layer_GPs: Fitted GP models for functions in the first layer node
+            second_layer_GPs: A Fitted GP model for the function in the second layer node
+        """
+        self.first_layer_GPs = first_layer_GPs
+        self.second_layer_GP = second_layer_GP
+        self.random_seeds_first_layer = random_seeds_first_layer
+        self.random_seed_second_layer = random_seed_second_layer
+        self.n_first_layer_nodes = n_first_layer_nodes
+        self.normal_lower = normal_lower
+        self.normal_upper =normal_upper
+        self.thompson_first_layer = [None for i in range(self.n_first_layer_nodes)]
+        for i in range(self.n_first_layer_nodes):
+            self.thompson_first_layer[i] = get_gp_samples(model=self.first_layer_GPs[i], num_outputs=1, n_samples=1)
+        self.thompson_second_layer = get_gp_samples(model=self.second_layer_GP, num_outputs=1, n_samples=1)
+
+    def forward(self, X: Tensor) -> Tensor:
+        ts_vals_first_layer = torch.tensor([])
+        for i in range(self.n_first_layer_nodes):
+            ts_vals_first_layer= torch.cat((ts_vals_first_layer,self.thompson_first_layer[i].forward(X=X)),dim=1)
+        ts_vals_first_layer_norm = ts_vals_first_layer.clone()
+        for i in range(self.n_first_layer_nodes):
+            ts_vals_first_layer_norm =(ts_vals_first_layer_norm[..., i] - normal_lower[i]) / (
+                        normal_upper[i] - normal_lower[i])
+        ts_val = self.thompson_second_layer.forward(X=ts_vals_first_layer_norm)
+        return ts_val
