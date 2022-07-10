ddd2f1e78a290448e0762b1f7a2987d3ec703707
poompol <104692189+poompol@users.noreply.github.com>
2022-07-10 17:09:40 -0400

    Update bofn_pt_trial.py

--- bofn_pt/bofn_pt_trial.py
+++ bofn_pt/bofn_pt_trial.py
@@ -19,6 +19,7 @@ from bofn_pt.models.gp_network_partial import GaussianProcessNetworkPartial
 from bofn_pt.utils.posterior_mean import PosteriorMean
 from bofn_pt.acquisition_function_optimization.optimize_acqf import optimize_acqf_and_get_suggested_point
 from bofn_pt.acquisition_function_optimization.thompson_fn_acqf import ThompsonSamplingFN
+from bofn_pt.acquisition_function_optimization.thompson_2nd_acqf import ThompsonSampling2ND
 from botorch.models.transforms import Standardize
 from botorch.optim.initializers import initialize_q_batch_nonneg
 
@@ -76,8 +77,10 @@ def bofn_pt_trial(
     init_batch_id = 1
     for iteration in range(init_batch_id, n_bo_iter+1):
         print("Experiment: " + problem)
-        print("First Batch algo: " + first_batch_algo)
-        print("Constant Liar: " + str(Constant_Liar))
+        if first_batch_algo is not None:
+            print("First Batch algo: " + first_batch_algo)
+        if Constant_Liar is not None:
+            print("Constant Liar: " + Constant_Liar)
         print("Second Batch algo: "+ second_batch_algo)
         print("Trial: " + str(trial))
         print("Iteration: " + str(iteration))
@@ -85,11 +88,15 @@ def bofn_pt_trial(
         t0 = time.time()
         objective_at_X=objective_at_X.reshape((len(objective_at_X),1))
 
-        if first_batch_algo== None:
+        if first_batch_algo == None and Constant_Liar == None:
             if second_batch_algo == "RAND":
                 second_batch = torch.rand([n_second_batch, input_dim])
-            elif second_batch_algo == "TS-Whole":
-                print("To be filled")
+            elif second_batch_algo == "TSWH":
+                second_layer_input = network_output_at_X[:,:-1].clone()
+                second_layer_output = objective_at_X.clone()
+                second_batch = get_second_batch_TS_whole(first_layer_input=first_layer_input,
+                first_layer_output=first_layer_output,second_layer_input=second_layer_input,
+                second_layer_output=second_layer_output,n_first_layer_nodes=n_first_layer_nodes,n_second_batch=n_second_batch)
             else:
                 print("Invalid algorithm")
                 break
@@ -140,6 +147,8 @@ def bofn_pt_trial(
                         CL = model_second_layer.posterior(y_at_new_point_norm).mean.item()
                         second_layer_input = torch.cat((second_layer_input,y_at_new_point),0)
                         second_layer_output = torch.cat((second_layer_output,torch.tensor(CL).reshape(1,1)),0)
+            print(temp_second_layer_input)
+            print(temp_second_layer_input.shape)
             second_batch = get_second_batch(first_batch = first_batch, temp_second_layer_input=temp_second_layer_input,
             model_second_layer=model_second_layer, best_obs_val = best_obs_val,
             n_second_batch = n_second_batch,second_batch_algo=second_batch_algo)
@@ -243,10 +252,7 @@ def get_second_batch(first_batch: Tensor,temp_second_layer_input: Tensor,model_s
             selected_tensor = temp_second_layer_input[X,:]
             fitness_val = qEI(selected_tensor).item()
             return fitness_val
-    elif second_batch_algo == "TS":
-        print("To be filled")
-
-    ga_instance = pygad.GA(num_generations=50,
+        ga_instance = pygad.GA(num_generations=50,
                            sol_per_pop=10,
                            num_genes=n_second_batch,
                            num_parents_mating=2,
@@ -254,48 +260,66 @@ def get_second_batch(first_batch: Tensor,temp_second_layer_input: Tensor,model_s
                            gene_type=int,
                            gene_space=gene_space,
                            allow_duplicate_genes=False)
-    ga_instance.run()
-    selected_indices = ga_instance.best_solution()[0]
-    new_second_batch = first_batch[selected_indices,:].clone()
+        ga_instance.run()
+        selected_indices = ga_instance.best_solution()[0]
+        new_second_batch = first_batch[selected_indices,:].clone()
+    elif second_batch_algo == "TS":
+        new_second_batch = torch.tensor([])
+        for i in range(n_second_batch):
+            acquisition_function = ThompsonSampling2ND(second_layer_GP=model_second_layer)
+            thompson_val = acquisition_function.forward(X=temp_second_layer_input)
+            arg_max_index = torch.argmax(thompson_val)
+            if i==0:
+                new_second_batch = first_batch[arg_max_index,:].clone().reshape(1,first_batch.shape[-1])
+            else:
+                new_second_batch = torch.cat((new_second_batch,
+                first_batch[arg_max_index,:].clone().reshape(1,first_batch.shape[-1])),dim=0)
+            temp_second_layer_input=torch.cat((temp_second_layer_input[:arg_max_index],
+            temp_second_layer_input[arg_max_index+1:]))
+            first_batch = torch.cat((first_batch[:arg_max_index],
+            first_batch[arg_max_index+1:]))
     return new_second_batch
 
-def generate_batch_thompson(
+def get_second_batch_TS_whole(
     first_layer_input: Tensor,
     first_layer_output: Tensor,
+    n_first_layer_nodes: int,
     second_layer_input: Tensor,
     second_layer_output: Tensor,
-    batch_size,
-    n_candidates,
-    network_to_objective_transform,
-    sampler="rff",  # "cholesky", "ciq", "rff"
-    use_keops=False,
+    n_second_batch: int,
 ):
-    assert sampler in ("cholesky", "ciq", "rff", "lanczos")
-    assert first_layer_input.min() >= 0.0 and first_layer_input.max() <= 1.0 and torch.all(torch.isfinite(second_layer_output))
-    # Fit a GP model
-    model = GaussianProcessNetworkPartial(train_X = first_layer_input,
-    train_Y_output_first_layer = first_layer_output,
-    train_Y_input_second_layer = second_layer_input,
-    train_Z = second_layer_output)
-    # Draw samples on a Sobol sequence
-    sobol = SobolEngine(first_layer_input.shape[-1], scramble=True)
-    X_cand = sobol.draw(n_candidates).to(dtype=dtype, device=device)
-    # Thompson sample
-    with ExitStack() as es:
-        if sampler == "cholesky":
-            es.enter_context(gpts.max_cholesky_size(float("inf")))
-        elif sampler == "ciq":
-            es.enter_context(gpts.fast_computations(covar_root_decomposition=True))
-            es.enter_context(gpts.max_cholesky_size(0))
-            es.enter_context(gpts.ciq_samples(True))
-            es.enter_context(gpts.minres_tolerance(2e-3))  # Controls accuracy and runtime
-            es.enter_context(gpts.num_contour_quadrature(15))
-        elif sampler == "lanczos":
-            es.enter_context(gpts.fast_computations(covar_root_decomposition=True))
-            es.enter_context(gpts.max_cholesky_size(0))
-            es.enter_context(gpts.ciq_samples(False))
-        elif sampler == "rff":
-            es.enter_context(gpts.fast_computations(covar_root_decomposition=True))
-        thompson_sampling = MaxPosteriorSampling(model=model, replacement=False,objective = network_to_objective_transform)
-        X_next = thompson_sampling(X_cand, num_samples=batch_size)
-    return X_next
+    input_dim = first_layer_input.shape[-1]
+    first_layer_GPs = [None for i in range(n_first_layer_nodes)]
+    for i in range(n_first_layer_nodes):
+        first_layer_GPs[i] = SingleTaskGP(train_X=first_layer_input,train_Y=first_layer_output[...,[i]],outcome_transform=Standardize(m=1))
+    second_layer_input_norm = second_layer_input.clone()
+    normal_lower = [None for i in range(n_first_layer_nodes)]
+    normal_upper = [None for i in range(n_first_layer_nodes)]
+    for i in range(n_first_layer_nodes):
+        normal_lower[i] = torch.min(second_layer_input[..., i])
+        normal_upper[i] = torch.max(second_layer_input[..., i])
+        second_layer_input_norm[..., i] = (second_layer_input_norm[..., i] - normal_lower[i]) / (
+                    normal_upper[i] - normal_lower[i])
+    second_layer_GP  = SingleTaskGP(train_X=second_layer_input_norm, train_Y=second_layer_output,
+                                        outcome_transform=Standardize(m=1))
+    for i in range(n_second_batch):
+        acquisition_function = ThompsonSamplingFN(first_layer_GPs=first_layer_GPs,
+                                                second_layer_GP=second_layer_GP,
+                                                n_first_layer_nodes=n_first_layer_nodes,
+                                                normal_lower=normal_lower,normal_upper=normal_upper)
+        test_x = torch.rand(1,input_dim)
+        new_point = optimize_acqf_and_get_suggested_point(
+            acq_func = acquisition_function,
+            bounds=torch.tensor([[0. for i in range(input_dim)],
+                                [1. for i in range(input_dim)]]),
+            batch_size=1,
+            batch_limit=1,
+            init_batch_limit=1
+        )
+        if i==0:
+            new_second_batch = new_point
+        else:
+            new_second_batch = torch.cat((new_second_batch,new_point),dim=0)
+        print("this is second batch")
+        print(new_second_batch)
+    return new_second_batch
